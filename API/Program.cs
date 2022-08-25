a747c9211d54aab4c04af58d634a0a033c2f220e
M.H. Mahmoudi <m.h.mahmoudi@blisdigital.com>
2022-08-25 12:13:52 +0200

    Part 01

---
+++ API/Program.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Persistence;
+
+namespace API
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            //-- to inject migration, should be put extra staff
+            //-- for "CreateHostBuilder(args)" in between Build() and Run()
+            // CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
+            try
+            {
+                var context = services.GetRequiredService<DataContext>();
+                await context.Database.MigrateAsync();
+                //-- injecting Activity seed to db for a first time
+                //-- because Seed.SeedData is async method, the other methods here should be async
+                await Seed.SeedData(context);
+            }
+            catch (Exception ex)
+            {
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(ex, "An Error Occurred during Migration");
+            }
+            await host.RunAsync();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
